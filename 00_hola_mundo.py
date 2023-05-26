c61121d8a0f5c5dd82d3e9ffb264d8e2e5c9df2f
angiovanny <66651933+angiovanny@users.noreply.github.com>
2023-05-25 21:48:16 -0500

    Add files via upload
    Upload file

---
+++ 00_hola_mundo.py
@@ -0,0 +1,18 @@
+# Hola mundo
+"""
+Comentario
+En
+Varias Linea
+"""
+'''
+Otro
+Comentario
+'''
+print("Hola Python")
+print('Hola Python')
+
+# type consulta el tipo de dato
+print(type('Hola Python'))       # Tipo de dato str
+print(type(5))                   # Tipo de dato int
+print(type(5.5))                 # Tipo de dato float
+print(type(T        rue))                # Tipo de dato bool
